a51a7ca2ffd09a1647f505a3bca602129c5f3003
Maciek Sakrejda <m.sakrejda@gmail.com>
2015-10-10 01:51:02 -0700

    More specs

--- spec/attr_pouch_spec.rb
+++ spec/attr_pouch_spec.rb
@@ -1,17 +1,20 @@
 require 'spec_helper'
 
 describe AttrPouch do
-  context "with a string attribute" do
-    let(:bepouched) do
-      Class.new(Sequel::Model(:items)) do
-        include AttrPouch
 
-        pouch(:attrs) do
-          field :foo, String
-        end
+  def make_pouchy(field_name, field_type, opts={})
+    bepouched = Class.new(Sequel::Model(:items)) do
+      include AttrPouch
+
+      pouch(:attrs) do
+        field field_name, field_type, opts
       end
     end
-    let(:pouchy) { bepouched.create }
+    bepouched.create
+  end
+
+  context "with a string attribute" do
+    let(:pouchy) { make_pouchy(:foo, String) }
 
     it "generates getter and setter" do
       expect(pouchy.foo).to be_nil
@@ -33,6 +36,49 @@ describe AttrPouch do
       expect(result).to_not be_nil
       pouchy.reload
       expect(pouchy.foo).to eq('bar')
+    end
+  end
+
+  context "with an integer attribute" do
+    let(:pouchy) { make_pouchy(:foo, Integer) }
+
+    it "preserves the type" do
+      pouchy.update(foo: 42)
+      pouchy.reload
+      expect(pouchy.foo).to eq(42)
+    end
+  end
+
+  context "with a float attribute" do
+    let(:pouchy) { make_pouchy(:foo, Float) }
+
+    it "preserves the type" do
+      pouchy.update(foo: 2.78)
+      pouchy.reload
+      expect(pouchy.foo).to eq(2.78)
+    end
+  end
+
+  context "with a boolean attribute" do
+    let(:pouchy) { make_pouchy(:foo, :bool) }
+
+    it "preserves the type" do
+      pouchy.update(foo: true)
+      pouchy.reload
+      expect(pouchy.foo).to be true
+    end
+  end
+
+  context "with a Sequel::Model attribute" do
+    let(:model_class) { Class.new(Sequel::Model(:items)) }
+    let(:pouchy)      { make_pouchy(:foo, model_class) }
+
+    it "preserves the type" do
+      new_model = model_class.create
+      pouchy.update(foo: new_model)
+      pouchy.reload
+      expect(pouchy.foo).to be_a(model_class)
+      expect(pouchy.foo.id).to eq(new_model.id)
     end
   end
 
